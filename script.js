db15d430bef52605c5054b641844e6694fd1498f
laurel-thomson <laurel.beth.thomson@gmail.com>
2020-02-27 08:26:39 -0700

    Adding content and working on observing for which section is current

--- script.js
+++ script.js
@@ -26,5 +26,29 @@ const navCallback = function(entries) {
     });
 }
 
-const navObserver = new IntersectionObserver(navCallback, {});
+const navObserver = new IntersectionObserver(navCallback, { threshold : 0.75 });
 navObserver.observe(header);
+
+const sections = document.querySelectorAll('h2');
+const navItems = document.querySelectorAll('nav a');
+
+const sectionCallback = function(entries) {
+    entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+            const id = entry.target.id + '-link';
+            navItems.forEach(function(navItem) {
+                if (navItem.id === id) {
+                    navItem.classList.add('active');
+                } else {
+                    navItem.classList.remove('active');
+                }
+            });
+        }
+    });
+}
+
+const sectionObserver = new IntersectionObserver(sectionCallback, {});
+sections.forEach(function(section) {
+    sectionObserver.observe(section);
+});
+
